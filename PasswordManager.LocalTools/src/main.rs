5a6c64cc2f92d17ed6828f354c2dde9ce2e22951
Vyron <pvyron@gmail.com>
2023-02-22 02:02:29 +0200

    add rust project for local tools

---
+++ PasswordManager.LocalTools/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let mut myage = 29;
+    let myname = "Vyron";
+    
+    println!("Hello {}, you are {} years old!", myname, myage);
+
+    myage = 30;
+    
+    println!("Hello {}, now you are {} years old!", myname, myage);
+}
